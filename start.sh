569188b8e8af1215ae18b990802eaa4d406181bb
CYH4157 <47914438+CYH4157@users.noreply.github.com>
2024-08-13 00:40:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 if [ -n "$TAIDE_API_KEY" ]; then
     echo "TAIDE_API_KEY detected, running app/app_taide.py..."
-    python app/app_taide.py
+    poetry run python app/app_taide.py
 elif [ -n "$NVIDIA_API_KEY" ]; then
     echo "NVIDIA_API_KEY detected, running app/app_nvidia.py..."
-    python app/app_nvidia.py
+    poetry run python app/app_nvidia.py
 else
     echo "No API key provided. Container will keep running."
     tail -f /dev/null
